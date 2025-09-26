b3b4108e22ace9763572f52b2865890a69659cd6
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2025-09-26 09:06:59 +0200

    Updated package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["VSMap"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/virtualstores/ios-foundation.git", .exact("1.9.1")),
+        .package(url: "https://github.com/virtualstores/ios-foundation.git", .exact("1.9.2")),
         .package(url: "https://github.com/mapbox/mapbox-maps-ios.git", .upToNextMinor(from: "10.18.0")),
     ],
     targets: [
